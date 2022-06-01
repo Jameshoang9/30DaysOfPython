f085a40668e452d836186c1326d6456253638720
JamesHoang <Jameshoang20@gmail.com>
2022-06-01 16:21:15 +0100

    Updating the exercises

---
+++ Day1/exercise 1.py
@@ -0,0 +1,35 @@
+#maths
+print(3+4)
+print(3*4)
+print(3//4)
+print(3**4)
+
+#printing out strings
+print('james')
+print('James Hoang)')
+print('I am enjoying 30 days of python')
+
+#Checking data types
+print(type(9.8))
+print(type(4-4j))
+print(type(['james','doni','uk']))
+print(type('james'))
+print(type('james hoang'))
+
+#booleans
+print(True)
+print(False)
+print(type(True))
+print(type(False))
+#make sure to capitalise True and False
+
+'''
+interger:5
+float:6.5,6.0
+complex:4-4j
+string:'james'
+boolean:True,False
+list:['1','2','3']
+tuple:('1','2','3')
+#tuple cannot be changed but a list can
+'''
---
+++ Day1/exercise 2.py
@@ -0,0 +1,6 @@
+#The two points are (2,3) and (10,8)
+#Formula is d(p,q)**2 = (q1-p1)**2 + (q2-p2)**2
+#p = point 1 and q = point 2 
+print(((10-2)**2+(8-3)**2)**0.5)
+#can use **0.5 **1/2 to square root 
+print(9**0.5)
