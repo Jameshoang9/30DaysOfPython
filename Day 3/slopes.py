aecebed1d6cd522292e0ac77111e4e3617f82e3d
JamesHoang <Jameshoang20@gmail.com>
2022-06-03 21:38:23 +0100

    updates slopes file

--- Day 3/slopes.py
+++ Day 3/slopes.py
@@ -1,5 +1,14 @@
 #8.Calculate the slope, x and y intercept of y = 2x - 2
+#formula - y = m*x + b
+# m = slope, b = y intercept
 
-x = 1
-y = 2 * x - 2
-print (y)
+slope = 2
+y_intercept = -2
+
+#when finding the x -intercept, y = 0
+# y = 2x - 2 --> 2 + y = 2x --> 2 = 2x --> 1 = x
+y = 0
+x_intercept = (slope + y)
+
+
+
